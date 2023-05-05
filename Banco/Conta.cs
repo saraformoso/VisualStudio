da63473cf646c33b9d8e13ce7930402f5701544c
saraformoso <saraformoso@gmail.com>
2023-05-05 09:33:12 +0100

    Criamos o projeto

---
+++ Banco/Conta.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco //aplicar estas funcionalidades de emcapsulamento...
+{
+    internal class Conta
+    { 
+        //Defenição de propriedades
+        public int Numero_Conta { get;} //obtenho o numero de conta...mas não posso alterar
+        public string Titular { get; set; } //posso obter e alterar
+        public double Saldo { get; private set; } //private set->interessa alterar o saldo mas dentro da classe atraves de operações matematicas
+        
+        
+        //Construtores
+
+        
+        //este construtor permite instanciar contas bancarias apenas com o numero de conta e o seu titular
+        public Conta(int NumeroConta, string NomeTitular)
+        {
+            Numero_Conta = NumeroConta;
+            Titular = NomeTitular;
+
+        }
+        //este construtor permite instanciar contas bancarias  com o numero de conta, seu titular e saldo
+        public Conta(int NumeroConta, string NomeTitular, double Saldo):this(NumeroConta, NomeTitular) 
+        {
+            Numero_Conta = NumeroConta;
+            Titular = NomeTitular;
+            Saldo = Saldo;
+        }
+
+
+        //Secção de Metodos
+
+        //metodo que permite depositar dinheiro na conta
+
+        public void Deposito(double quantia) 
+        {
+        Saldo += quantia;
+        }
+
+        // metodo que permite levantar dinheiro da conta
+
+        public void Levantamento(double quantia) 
+        {
+        Saldo -= quantia + 5.00;
+
+        }
+
+      //este metodo imprime na tela os dados pretendidos
+
+        public override string ToString()
+        {
+            return "Conta Bancaria numero"
+                + Numero_Conta
+                + ", Titular da conta: "
+                + Titular
+                + ", Saldo:"
+                + Saldo.ToString("F2")
+                +"euros.";
+
+
+        }
+
+
+
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,50 @@
+﻿using Banco;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Conta cb;
+
+      
+
+
+        Console.WriteLine("Entre o numero de conta: ");
+        int numero=int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entre o titular da conta: ");
+        string titular = Console.ReadLine();
+
+        Console.WriteLine("Haverá deposito inicial?:S/N ");
+        char resposta = char.Parse(Console.ReadLine());
+        if (resposta == 'S' || resposta == 's')
+        {
+            Console.WriteLine("Entre o valor de deposito:");
+            double depositoInicial = double.Parse(Console.ReadLine());
+            cb = new Conta(numero, titular, depositoInicial);
+        }
+        else {
+            cb = new Conta(numero, titular);
+        }
+        Console.WriteLine();
+        Console.WriteLine("dados da conta: ");
+        Console.WriteLine(cb);
+        Console.WriteLine();
+        Console.WriteLine("Valor a depositar");
+        double quantia = double.Parse(Console.ReadLine());
+        cb.Deposito(quantia);
+        Console.WriteLine("Dados actualizados");
+        Console.WriteLine(cb);
+
+        Console.WriteLine();
+        Console.WriteLine("Valor a levantar");
+        quantia = double.Parse(Console.ReadLine());
+        cb.Levantamento(quantia);
+        Console.WriteLine();
+        Console.WriteLine("Dados actualizados");
+        Console.WriteLine(cb);
+
+        //cb.Titular = "Maria";
+        //Console.WriteLine();
+    }
+}
